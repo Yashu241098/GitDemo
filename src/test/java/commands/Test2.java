e59993274af79e7fc8880171e18bb930ca88b97f
yashu <yeswanthappala1998@gmail.com>
2023-03-27 23:12:38 +0530

    this

--- src/test/java/commands/Test2.java
+++ src/test/java/commands/Test2.java
@@ -1,5 +1,25 @@
 package commands;
 
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class Test2 {
 
+	@BeforeTest
+	public void Initializedriver()
+	{
+		System.out.println("First we are initialize browser");
+		System.out.println("so, we use before test annotate");
+		System.out.println("then we write normal test cases");
+				
+	}
+	
+	@Test
+	public void Testcase() {
+		
+		System.out.println("we are writting testcases in this annotation");
+		System.out.println("for automation we are using Rest Assured testing");
+		System.out.println("There are num of testing tools");
+		System.out.println("we using web automation");
+	}
 }
