f910a9d639dc5df018695f5f0c17ffd987ae1710
yashu <yeswanthappala1998@gmail.com>
2023-03-27 23:03:14 +0530

    Successfully created main method

---
+++ src/test/java/commands/Test3.java
@@ -0,0 +1,10 @@
+package commands;
+
+public class Test3 {
+
+	public static void main(String args[]) {
+		
+		System.out.println("This is Final Code");
+		System.out.println("so, we using main method to run test cases");
+	}
+}
