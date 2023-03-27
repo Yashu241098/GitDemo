eb16e50042c3bff57627851e7ce0c02031f46b85
yashu <yeswanthappala1998@gmail.com>
2023-03-27 21:25:40 +0530

    Added files in GitHub folder

---
+++ src/test/java/commands/Test1.java
@@ -0,0 +1,15 @@
+package commands;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void Demo1() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This class is used for Demo of GitHub Folder");
+		System.out.println("There are many courses in college");
+		System.out.println("This is Demo1 for Selenium Automation testing");
+	}
+}
